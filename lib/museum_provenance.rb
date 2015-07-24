291e542f8c41b02986c15b41d6301be8bc0de2c8
Jeff Chiu <jeff@codefork.com>
2015-07-24 15:22:12 -0400

    require lib files in alphabetical order, to guarantee same behavior on Linux as on Mac OS

--- lib/museum_provenance.rb
+++ lib/museum_provenance.rb
@@ -7,7 +7,7 @@ require 'date_time_precision/format/iso8601'
 require 'date_time_precision/format/string'
 require 'active_support/core_ext/integer/inflections'
 
-Dir["#{File.dirname(__FILE__)}/museum_provenance/**/*.rb"].each { |f| require(f) }
+Dir["#{File.dirname(__FILE__)}/museum_provenance/**/*.rb"].sort.each { |f| require(f) }
 
 # MuseumProvenance is a general-purpose library for cultural institutions 
 # @todo Write this, please.
